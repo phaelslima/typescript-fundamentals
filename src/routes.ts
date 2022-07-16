54f71f7e0d28eb0f291ff2c6464495853caa7e54
Raphael Lima <phaelslima@gmail.com>
2022-07-16 09:14:42 -0300

    feat: add first service

---
+++ src/routes.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+import CreateCourseService from "./CreateCourseService";
+
+export function createCourse(request: Request, response: Response) {
+  CreateCourseService.execute({
+    name: "NodeJS",
+    educator: "Dani"
+  });
+
+  return response.send();
+}
